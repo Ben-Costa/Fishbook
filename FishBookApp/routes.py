da506b297baeaacf437894e1af6b0d3d820b0c1a
Ben-Costa <bc234cc@gmail.comm>
2021-07-26 00:10:08 +0000

    Created flask infrastructure to run local host sever for developement

---
+++ FishBookApp/routes.py
@@ -0,0 +1,8 @@
+from FishBookApp import app
+from flask import render_template, redirect, url_for
+
+@app.route('/')
+@app.route('/home')
+def home_page():
+    return render_template('base.html')
+
